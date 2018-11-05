d6c44013e34dd608db2a38f03e0cca8fb54d6cdd
TK <leandrotk100@gmail.com>
2018-11-05 14:01:34 -0200

    Add more ruby code for talk

--- higher_order_functions/higher_order_function.rb
+++ higher_order_functions/higher_order_function.rb
@@ -45,3 +45,32 @@ people_sentences
 # ['TK has 26 years old', 'Kaio has 10 years old', 'Kazumi has 30 years old']
 
 people.map { |person| build_sentence(person) }
+
+# reduce
+prices_list = [2.0, 1, 2.5, 10, 14.5]
+
+sum_of_all = 0
+
+prices_list.each { |price| sum_of_all += price }
+
+sum_of_all # 30.0
+
+prices_list.reduce(:+) # 30.0
+prices_list.reduce(70, :+) # 100.0
+
+orders = [
+  { title: 'P1', price: 10 },
+  { title: 'P2', price: 30 },
+  { title: 'P3', price: 20 },
+  { title: 'P4', price: 60 }
+]
+
+orders.reduce(0) do |sum, order|
+  sum + order[:price]
+end
+
+square = -> object {
+  object * object
+}
+
+[1, 2, 3].map(&square)
